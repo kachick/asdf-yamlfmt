1aff3505cc9b0bc8a09f39fe06481222994f74bb
Kenichi Kamiya <kachick1@gmail.com>
2024-03-23 10:59:22 +0900

    Use globstar only in scripts/ again

--- scripts/format.bash
+++ scripts/format.bash
@@ -2,8 +2,11 @@
 
 set -euxo pipefail
 
+# This option don't work in old bash as 3.x that installed in macOS
+shopt -s globstar
+
 shfmt --language-dialect bash --write \
-	./**/*
+	./**/*.bash bin/**/*
 
 dprint fmt
 
